6dc3f8c3423b9f72ef502e2fb9bcf3ab0050fd6d
Dimitris Exadaktylos <jimexa@yahoo.gr>
2021-11-09 09:06:48 +0200

    add basic components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
 import SearchBar from './components/SearchBar';
+import {
+    BodyContainer,
+    HeaderContainer,
+    MovieDesc,
+    MoviesList,
+} from './components/styled';
+
+const Header = () => {
+    return (
+        <HeaderContainer>
+            <button>Sort by</button>
+            <SearchBar />
+        </HeaderContainer>
+    );
+};
+
+const Body = () => {
+    return (
+        <BodyContainer>
+            <MoviesList />
+            <MovieDesc />
+        </BodyContainer>
+    );
+};
 
 function App() {
-    return <SearchBar />;
+    return (
+        <>
+            <Header />
+            <Body />
+        </>
+    );
 }
 
 export default App;
--- src/components/styled.js
+++ src/components/styled.js
@@ -3,3 +3,14 @@ import styled from '@emotion/styled';
 export const StyledSSearchBar = styled('input')({
     color: 'red',
 });
+
+export const HeaderContainer = styled('div')({
+    backgroundColor: '#F7F8FA',
+});
+
+export const BodyContainer = styled('div')({
+    display: 'flex',
+});
+
+export const MoviesList = styled('div')({});
+export const MovieDesc = styled('div')({});
