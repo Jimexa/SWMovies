44c20b2495fc82ec73d3a36c91eea991d860695d
Dimitris Exadaktylos <jimexa@yahoo.gr>
2021-11-11 00:02:00 +0200

    add a simple test

---
+++ src/components/DropDown/DropDown.test.js
@@ -0,0 +1,22 @@
+import { render, screen } from '@testing-library/react';
+import DropDown from './DropDown';
+import { AppContext } from '../../App';
+
+const sortMoviesByYear = () => {
+    console.log('sortbyyear');
+};
+const sortMoviesByEpisode = () => {
+    console.log('sortbyepisode');
+};
+
+test('renders dropdown', () => {
+    render(
+        <AppContext.Provider
+            value={(sortMoviesByEpisode, sortMoviesByYear)}
+        >
+            <DropDown />
+        </AppContext.Provider>,
+    );
+    const sortByElement = screen.getByText('Sort by');
+    expect(sortByElement).toBeInTheDocument();
+});
