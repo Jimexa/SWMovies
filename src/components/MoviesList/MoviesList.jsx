89ab18eed5ad4aabd0b5669b11652ba01f437146
Dimitris Exadaktylos <jimexa@yahoo.gr>
2021-11-10 22:30:00 +0200

    finished main functionality

--- src/components/MoviesList/MoviesList.jsx
+++ src/components/MoviesList/MoviesList.jsx
@@ -7,25 +7,34 @@ import {
 } from './styled';
 
 export default function MoviesList() {
-    const { moviesData, handleMovieClick } = useContext(AppContext);
+    const { moviesData, handleMovieClick, searchTerm } =
+        useContext(AppContext);
 
     return (
         <MoviesListContainer>
             {moviesData
-                ? moviesData.map((movie) => {
-                      return (
-                          <MoviesListItem
-                              onClick={() => handleMovieClick(movie)}
-                              key={movie.episode_id}
-                          >
-                              <Episode>
-                                  EPISODE {movie?.episode_id}
-                              </Episode>
-                              <span>{movie?.title}</span>
-                              <span>{movie?.release_date}</span>
-                          </MoviesListItem>
-                      );
-                  })
+                ? moviesData
+                      .filter((movie) =>
+                          movie.title
+                              .toUpperCase()
+                              .includes(searchTerm.toUpperCase()),
+                      )
+                      .map((movie) => {
+                          return (
+                              <MoviesListItem
+                                  onClick={() =>
+                                      handleMovieClick(movie)
+                                  }
+                                  key={movie.episode_id}
+                              >
+                                  <Episode>
+                                      EPISODE {movie?.episode_id}
+                                  </Episode>
+                                  <span>{movie?.title}</span>
+                                  <span>{movie?.release_date}</span>
+                              </MoviesListItem>
+                          );
+                      })
                 : 'loading'}
         </MoviesListContainer>
     );
